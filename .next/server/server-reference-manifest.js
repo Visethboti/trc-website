97034da06781f00fec07f7559c9ba296e620e043
PonlokTTR <ponlokpov2@gmail.com>
2024-09-16 17:13:34 +0700

    feat: add about-TRC page

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,2 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"n/uVkATKZ8A6DNMf/QOy2w59F9ONrZRelwsrxqvC8GI=\"\n}"
+self.__RSC_SERVER_MANIFEST =
+  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "n/uVkATKZ8A6DNMf/QOy2w59F9ONrZRelwsrxqvC8GI="\n}';
--- .next/static/webpack/webpack.ac0e9ec8e4e93a51.hot-update.js
+++ .next/static/webpack/webpack.ac0e9ec8e4e93a51.hot-update.js
@@ -7,12 +7,19 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"]("webpack",{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ /* webpack/runtime/getFullHash */
-/******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "42f35af1a9a2cd42"; }
-/******/ }();
-/******/ 
-/******/ }
+self["webpackHotUpdate_N_E"](
+  "webpack",
+  {},
+  /******/ function (__webpack_require__) {
+    // webpackRuntimeModules
+    /******/ /* webpack/runtime/getFullHash */
+    /******/ !(function () {
+      /******/ __webpack_require__.h = function () {
+        return "42f35af1a9a2cd42";
+      };
+      /******/
+    })();
+    /******/
+    /******/
+  },
 );
