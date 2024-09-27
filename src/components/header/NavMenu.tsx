bf6fd3e083f76a5b2f3b47e19b2a810591ef766c
notFound217 <rsmey8373@gmail.com>
2024-09-27 11:16:48 -0400

    fix: change navbar component

--- src/components/header/NavMenu.tsx
+++ src/components/header/NavMenu.tsx
@@ -1,153 +1,199 @@
-import { ListItem } from "./ListItem";
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { ChevronDown } from "lucide-react"
 
-export const NavMenu = () => {
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function NavMenu() {
   return (
-    <NavigationMenu.Root className="relative z-10 hidden w-full flex-1 items-center justify-center lg:flex">
-      <NavigationMenu.List className="center m-0 flex list-none rounded-[6px] bg-white p-1">
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-            Laws & Regulartions{" "}
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute left-0 top-0 w-full sm:w-auto">
-            <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
-              <ListItem href="https://stitches.dev/" title="Law"></ListItem>
-              <ListItem href="/colors" title="Royal Decrees"></ListItem>
-              <ListItem
-                href="https://icons.radix-ui.com/"
-                title="Sub Decrees"
-              ></ListItem>
-              <ListItem href="/" title="Circulation"></ListItem>
-              <ListItem href="/" title="Prakas"></ListItem>
-              <ListItem href="/" title="Decisions"></ListItem>
-              <ListItem href="/" title="Guidelines"></ListItem>
-            </ul>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
+    <div className="relative z-10 hidden w-full flex-1 items-center justify-center gap-2 xl:flex">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">
+            Laws & Regulartion
+            <ChevronDown className="mr-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-44 flex items-center justify-center pt-4 pb-4">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>Laws</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Royal Decrees</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Sub Decrees</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Circulations</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Prakas</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Decisions</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Guidelines</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-            Media{" "}
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute left-0 top-0 w-full sm:w-auto">
-            <ul className="m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[200px] sm:grid-flow-col sm:grid-rows-3">
-              <ListItem
-                title="News Releases"
-                href="/primitives/docs/overview/introduction"
-              ></ListItem>
-              <ListItem
-                title="Activities"
-                href="/primitives/docs/overview/introduction"
-              ></ListItem>
-              <ListItem
-                title="Events"
-                href="/primitives/docs/overview/introduction"
-              ></ListItem>
-            </ul>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">
+            Media
+            <ChevronDown className="mr-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40 flex items-center justify-center pt-4 pb-4">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>News Releases</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Activities</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Events</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-            Resources{" "}
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute left-0 top-0 w-full sm:w-auto">
-            <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
-              <ListItem
-                href="https://stitches.dev/"
-                title="Publications"
-              ></ListItem>
-              <ListItem href="/colors" title="Telecom Statistics"></ListItem>
-              <ListItem
-                href="https://icons.radix-ui.com/"
-                title="Active Operator"
-              ></ListItem>
-              <ListItem href="/" title="Mobile Prefixes"></ListItem>
-              <ListItem href="/" title="Common Codes"></ListItem>
-              <ListItem href="/" title="Emergency Numbers"></ListItem>
-              <ListItem href="/" title="Qualified Import Agents"></ListItem>
-              <ListItem href="/" title="Tower Map"></ListItem>
-            </ul>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">
+            Resources
+            <ChevronDown className="mr-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 flex items-center justify-center pt-4 pb-4">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>Publications</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Telecom Statistics</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Active Operators</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Mobile Prefixes</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Common Codes</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Emergency Numbers</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Qualified Import Agents</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Tower Map</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-            About{" "}
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute left-0 top-0 w-full sm:w-auto">
-            <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[500px] sm:grid-cols-[0.75fr_1fr]">
-              <ListItem
-                href="https://stitches.dev/"
-                title="Chairman's Keynoted"
-              ></ListItem>
-              <ListItem href="/colors" title="Vision & Mission"></ListItem>
-              <ListItem
-                href="https://icons.radix-ui.com/"
-                title="About TRC"
-              ></ListItem>
-              <ListItem href="/" title="TRC Leaders"></ListItem>
-              <ListItem href="/" title="TRC Structure"></ListItem>
-            </ul>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
+       <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">
+            About
+            <ChevronDown className="mr-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 flex items-center justify-center pt-4 pb-4">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span>Chairman's Keynote</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Vision & Mission</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>About TRC</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>TRC Leader</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>TRC Structure</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            className="text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-            href="https://github.com/radix-ui"
-          >
-            Contact
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">Contact</Button>
+        </DropdownMenuTrigger>
+      </DropdownMenu>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="text-violet11 hover:bg-violet3 focus:shadow-violet7 group flex select-none items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-            Service{" "}
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="data-[motion=from-start]:animate-enterFromLeft data-[motion=from-end]:animate-enterFromRight data-[motion=to-start]:animate-exitToLeft data-[motion=to-end]:animate-exitToRight absolute left-0 top-0 w-full sm:w-auto">
-            <ul className="one m-0 grid list-none gap-x-[10px] p-[22px] sm:w-[600px] sm:grid-cols-[0.75fr_1fr]">
-              <ListItem href="https://stitches.dev/" title="Public"></ListItem>
-              <ListItem href="/colors" title="Domain .KH"></ListItem>
-              <ListItem
-                href="https://icons.radix-ui.com/"
-                title="Internet Cafe"
-              ></ListItem>
-              <ListItem href="/" title="Telecom Equipment Shop"></ListItem>
-              <ListItem href="/" title="Operaor"></ListItem>
-              <ListItem href="/" title="Operational License"></ListItem>
-              <ListItem href="/" title="Spectrum License"></ListItem>
-              <ListItem href="/" title="Telecom Numbering Permit"></ListItem>
-              <ListItem href="/" title="Equipment TA Certificate"></ListItem>
-              <ListItem
-                href="/"
-                title="Qualified Agent Certification"
-              ></ListItem>
-              <ListItem
-                href="/"
-                title="Equipment Importation & Distribution Permit"
-              ></ListItem>
-              <ListItem href="/" title="Tower Construction Permit"></ListItem>
-              <ListItem
-                href="/"
-                title="Fiber Cable Construction Permit"
-              ></ListItem>
-              <ListItem
-                href="/"
-                title="Frequency Interference Complaint"
-              ></ListItem>
-            </ul>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Indicator className="data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]">
-          <div className="relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-primary" />
-        </NavigationMenu.Indicator>
-      </NavigationMenu.List>
-
-      <div className="perspective-[2000px] absolute left-0 top-full flex w-full justify-center">
-        <NavigationMenu.Viewport className="data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
-      </div>
-    </NavigationMenu.Root>
-  );
-};
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex items-center justify-center border-none focus:border-none">
+            Service
+            <ChevronDown className="mr-2 h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-80 flex items-center justify-center">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <span className="w-20 h-8 flex items-center justify-center rounded bg-primary text-white">Public</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Domain .KH</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Internet Cafe</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Telecom Equipment Shop</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span className="w-24 h-8 flex items-center justify-center rounded bg-primary text-white">Operator</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Operational License</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Spectrum License</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Telecom Numbering Permit</span>
+            </DropdownMenuItem>
+             <DropdownMenuItem>
+              <span>Equipment TA Certificate</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Qualified Agent Certification</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Equipment Importation & Distribution Permit</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Tower Construction Permit</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Fiber Cable Construction Permit</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Frequency Interference Complaint</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
