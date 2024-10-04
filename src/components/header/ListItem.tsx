b1b108aac7e76472b15ee45dc67aea10a27966d7
Boti <s.visethboti@gmail.com>
2024-10-04 22:48:13 +0700

    feat: add line height for drop down item

--- src/components/header/ListItem.tsx
+++ src/components/header/ListItem.tsx
@@ -19,7 +19,7 @@ export const ListItem = React.forwardRef<
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
+          <div className="text-sm font-medium leading-tight">{title}</div>
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
