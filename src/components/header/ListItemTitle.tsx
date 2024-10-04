c5a2de7c9eafe46d01af89165094e876277d63a0
Boti <s.visethboti@gmail.com>
2024-10-04 23:03:57 +0700

    feat: refactor ListItemTitle

--- src/components/header/ListItemTitle.tsx
+++ src/components/header/ListItemTitle.tsx
@@ -1,22 +1,18 @@
-import React from "react";
-
 import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
 
-export const ListItemTitle = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ title, children }) => {
+interface ListItemTitleProps {
+  title: string;
+}
+
+export const ListItemTitle: React.FC<ListItemTitleProps> = ({ title }) => {
   return (
     <li>
       <NavigationMenuLink className="flex items-center" asChild>
         <div className="my-2 rounded-md bg-primary px-4 py-3 text-white">
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
+          <p className="text-sm font-medium leading-none">{title}</p>
         </div>
       </NavigationMenuLink>
     </li>
   );
-});
+};
 ListItemTitle.displayName = "ListItemTitle";
