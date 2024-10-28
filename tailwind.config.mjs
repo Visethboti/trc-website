b338ef22108796c525d17175642541bb09075ac0
LAY-VICHA <115967466+LAY-VICHA@users.noreply.github.com>
2024-10-28 16:30:46 +0700

    Fix network image law media pages (#98)
    * feat: fix network pattern image in laws and media pages and border width
    
    * fix: fix format with prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -37,6 +37,7 @@ export default {
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
+        textcolor: "hsl(var(--text))",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
