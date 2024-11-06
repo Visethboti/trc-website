9dfa458321652a894a62e1eaa31ef288dd5d135d
LAY-VICHA <115967466+LAY-VICHA@users.noreply.github.com>
2024-11-06 23:12:30 +0700

    feat: fix leader page transition and network pattern and fix scroll spy highlight (#120)

--- src/components/page/structure/ChairmanCard.tsx
+++ src/components/page/structure/ChairmanCard.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import TextElement from "@/components/TextElement";
 
 import networkPattern from "@/asset/network-pattern.png";
-import { Fade } from "react-awesome-reveal";
+import { Fade, Slide } from "react-awesome-reveal";
 
 interface ChairmanCardProps {
   title: string;
@@ -19,10 +19,7 @@ const ChairmanCard = ({ children, title, name, image }: ChairmanCardProps) => {
     setIsVisible(!isVisible);
   };
 
-  const card =
-    "relative flex flex-col lg:flex-row cursor-pointer items-center justify-center gap-0 p-20 lg:gap-16 overflow-hidden transition-all duration-500 ease-out";
-  const cardImage = "transition-all duration-500 ease-out";
-  const cardContent = "overflow-hidden transition-all duration-500 ease-out";
+  const cardContent = "overflow-hidden transition-all duration-1000 ease-out";
   const networkPatternStyle = "absolute bottom-0 w-2/5";
   const networkPatternStyleExpanded = "right-[87%]";
 
@@ -36,14 +33,18 @@ const ChairmanCard = ({ children, title, name, image }: ChairmanCardProps) => {
           handleClick();
         }
       }}
-      className={`overflow-hidden ${card} `}
+      className="relative flex cursor-pointer flex-col items-center justify-center gap-0 overflow-hidden p-20 transition-all duration-500 ease-out lg:flex-row lg:gap-16"
     >
-      <div>
-        <figure className={`${cardImage} flex-col gap-5`}>
+      <Slide
+        direction="left"
+        duration={1000}
+        className={`${isVisible ? "hidden" : "flex"}`}
+        triggerOnce
+      >
+        <figure className="flex-col gap-5">
           <TextElement
             variant="subheading"
-            className="py-5 text-center text-yellow-500"
-            onClick={() => console.log("hello hgfghhgf")}
+            className="py-5 text-center text-secondary"
           >
             Chairman
           </TextElement>
@@ -59,10 +60,35 @@ const ChairmanCard = ({ children, title, name, image }: ChairmanCardProps) => {
             <span className="ml-3">{name}</span>
           </figcaption>
         </figure>
-      </div>
+      </Slide>
 
       {isVisible && (
-        <Fade className={`animate__fadeIn ${cardContent}`}>{children}</Fade>
+        <div className="relative flex cursor-pointer flex-col items-center justify-center gap-0 overflow-hidden transition-all duration-500 ease-out lg:flex-row lg:gap-16">
+          <Slide duration={1000} direction="right" triggerOnce>
+            <figure className="flex-col gap-5">
+              <TextElement
+                variant="subheading"
+                className="py-5 text-center text-secondary"
+              >
+                Chairman
+              </TextElement>
+              <div className="relative size-60 overflow-hidden rounded-full drop-shadow-lg dark:border-gray-300">
+                <img
+                  src={image.src}
+                  alt={`${title}${name}`}
+                  className="size-full object-cover"
+                />
+              </div>
+              <figcaption className="py-4 text-center font-bold text-white md:text-xl">
+                {title}
+                <span className="ml-3">{name}</span>
+              </figcaption>
+            </figure>
+          </Slide>
+          <Fade triggerOnce className={`animate__fadeIn ${cardContent}`}>
+            {children}
+          </Fade>
+        </div>
       )}
       <figure
         className={`${networkPatternStyle} ${isVisible ? networkPatternStyleExpanded : "left-0"}`}
@@ -72,6 +98,7 @@ const ChairmanCard = ({ children, title, name, image }: ChairmanCardProps) => {
           alt="Gradient Network"
           loading="eager"
           sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+          className="-scale-x-100 opacity-50"
         />
       </figure>
     </div>
