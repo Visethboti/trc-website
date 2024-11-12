94c5c634bfbedfb529cf8bdefd4efbf13fdef0ba
Boti <visethboti@gmail.com>
2024-11-12 11:50:29 +0700

    feat: config vercel url when undefined

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://astro.build/config
 export default defineConfig({
   site:
-    `https://${process.env.PUBLIC_SITE_URL}` ||
-    `https://${process.env.VERCEL_URL}` ||
-    "http://localhost:4321",
+    process.env.PUBLIC_SITE_URL || process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:4321",
   integrations: [
     react(),
     tailwind({
