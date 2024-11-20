cf8b2bf1b7b216bcffa14cfb5eaedf0e9eac0961
PonlokTTR <ponlokpov2@gmail.com>
2024-11-20 15:09:31 +0700

    feat: change footer and contact (#143)

--- src/components/footer/SocialMedia.tsx
+++ src/components/footer/SocialMedia.tsx
@@ -2,9 +2,17 @@ import { FacebookIcon, TelegramIcon, YouTubeIcon } from "./SocialMediaLinks";
 
 export default function SocialMedia() {
   const socialMediaLinks = [
-    { Icon: FacebookIcon, label: "Facebook", link: "/" },
+    {
+      Icon: FacebookIcon,
+      label: "Facebook",
+      link: "https://www.facebook.com/www.trc.gov.kh",
+    },
     { Icon: TelegramIcon, label: "Telegram", link: "/" },
-    { Icon: YouTubeIcon, label: "Youtube", link: "/" },
+    {
+      Icon: YouTubeIcon,
+      label: "Youtube",
+      link: "https://www.youtube.com/channel/UCEOuHij9GX1tozXg4UEr0Dg",
+    },
   ];
 
   return (
