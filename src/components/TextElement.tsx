649b0e09fa5539906d572b8857d91f23fe295b85
lay_vicha <layvicha081783923@gmail.com>
2024-12-02 11:59:50 +0700

    feat: change approval type name to equipment approval type and add line height to text element

--- src/components/TextElement.tsx
+++ src/components/TextElement.tsx
@@ -7,7 +7,7 @@ const CONFIG = {
   },
   subheading: {
     tag: "h2",
-    tw: "scroll-m-20 text-xl lg:text-2xl xl:text-3xl font-bold tracking-wide text-text font-khmerMEF2",
+    tw: "scroll-m-20 text-xl lg:text-2xl xl:text-3xl font-bold tracking-wide text-text leading-relaxed font-khmerMEF2",
   },
   smallheading: {
     tag: "h2",
@@ -15,7 +15,7 @@ const CONFIG = {
   },
   title: {
     tag: "h3",
-    tw: "scroll-m-20 text-lg lg:text-xl font-bold tracking-normal text-text",
+    tw: "scroll-m-20 text-lg lg:text-xl font-bold tracking-normal text-text leading-relaxed",
   },
   mediumtitle: {
     tag: "h3",
@@ -23,7 +23,7 @@ const CONFIG = {
   },
   smalltitle: {
     tag: "h4",
-    tw: "scroll-m-20 text-16 font-bold tracking-normal text-text",
+    tw: "leading-6 scroll-m-20 text-16 font-bold tracking-normal text-text",
   },
   large: {
     tag: "p",
@@ -31,7 +31,7 @@ const CONFIG = {
   },
   medium: {
     tag: "p",
-    tw: "leading-12 text-xl lg:text-2xl xl:text-4xl text-text",
+    tw: "text-xl lg:text-2xl xl:text-4xl text-text",
   },
   paragraph: {
     tag: "p",
